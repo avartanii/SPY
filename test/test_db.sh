36b9a2625f921530ea38995c1448be63d054c1cd
cf7 <cfranco7@lion.lmu.edu>
2017-04-08 22:06:10 -0700

    test build on travis-ci

--- test/test_db.sh
+++ test/test_db.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 echo 'Setting Environment Variables'
-if [ -f node_modules/.bin/../../set_test_env.sh ]; then chmod 700 node_modules/.bin/../../set_test_env.sh; source set_test_env.sh; else echo "No set_test_env.sh file"; fi;
+if [ -f set_test_env.sh ]; then chmod 700 set_test_env.sh; source set_test_env.sh; else echo "No set_test_env.sh file"; fi;
 echo 'Deleting db & logs';
 rm -rf db;
 rm -rf log;
 mkdir db;
 mkdir log;
 echo 'Initializing db';
-initdb -U $TEST_DB_USERNAME -D $(npm bin)/../../db; # make sure username is same as config user that owns the database
-pg_ctl start -D node_modules/.bin/../../db -l node_modules/.bin/../../log/postgres.log;
+initdb -U $TEST_DB_USERNAME -D db; # make sure username is same as config user that owns the database
+pg_ctl start -D db -l log/postgres.log;
 sleep 4;
 createdb -O $TEST_DB_USERNAME -U $TEST_DB_USERNAME $TEST_DB; # bash does not parse --username=$TEST_DB_USERNAME correctly without the space
-# psql -f node_modules/.bin/../../config/create_spy_test.sql -d postgres --username=$TEST_DB_USERNAME; # need to create the new database from within the default postgres database that is initialized first
-psql -f node_modules/.bin/../../db_setup/db.sql -d $TEST_DB -U $TEST_DB_USERNAME;
+# psql -f config/create_spy_test.sql -d postgres --username=$TEST_DB_USERNAME; # need to create the new database from within the default postgres database that is initialized first
+psql -f db_setup/db.sql -d $TEST_DB -U $TEST_DB_USERNAME;
 echo 'Test Database initialized and started';
 # connect to db $TEST_DB as user $TEST_DB_USERNAME
 # $ psql -d $TEST_DB -U $TEST_DB_USERNAME
